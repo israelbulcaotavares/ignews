f2947aeb86d72f7c10cb7f8052d9d4630f287582
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-05-29 01:54:56 -0300

    Autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,34 @@
-import { FaGithub  } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from "next-auth/react"
 
 import styles from './styles.module.scss'
 
 
-export function SignInButtom(){
-    const isUserLoggedIn = true;
+export function SignInButtom() {
 
-    return isUserLoggedIn ? (
-        <button type="button"
-        className={styles.signInButton}
+    const { data: session, status } = useSession()
+    const loading = status === "loading"
+
+    console.log(session);
+
+    return session ? (
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut() }
         >
-             <FaGithub color='#04d361'/>
-            Israel Tavares
+            <FaGithub color='#04d361' />
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
-    ):(
-        <button type="button"
-        className={styles.signInButton}
+    ) : (
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
-             <FaGithub color='#eba417'/>
+            <FaGithub color='#eba417' />
             Sign in with Github
         </button>
     )
