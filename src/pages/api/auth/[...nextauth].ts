4780b0713a3b7c0dca261022be833b730b0650b7
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-06-01 00:10:35 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,45 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth({ 
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization:{
-          params:{
-              scope:'read:user',
-          }
+      authorization: {
+        params: {
+          scope: 'read:user',
+        }
       }
-      
     }),
     // ...add more providers here
   ],
+
+  
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      }catch{
+        return false
+      }
+
+     
+    },
+  }
 })
 
 //FaunaDB - HTTP
