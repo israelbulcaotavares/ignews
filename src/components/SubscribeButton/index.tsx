162ee9ba3f55df6705961c5e932276cb907be8f8
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-06-02 01:03:51 -0300

    Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
-import { stripe } from '../../services/stripe';
+import { api } from '../../services/api'; 
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 
@@ -8,15 +9,27 @@ interface SusbcribeButtonProps {
 
 }
 export function SubscribeButton({ priceId }: SusbcribeButtonProps) {
-    const  {data: session}  = useSession();
+    const { data: session } = useSession();
 
 
-    function handleSubscrible() {
+   async function handleSubscrible() {
         if (!session) {
             signIn('github')
             return;
         }
 
+        try {
+            const response = await  api.post('/subscribe')
+
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJs()
+
+            stripe.redirectToCheckout({sessionId})
+
+        } catch(err) {
+            alert(err.message)
+        }
         //criação da checkout session
         //stripe.checkout.sessions.create
 
