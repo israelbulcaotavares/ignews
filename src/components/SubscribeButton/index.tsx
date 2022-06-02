302e9f5c220916416aa6307ce4b403f23fc6eaa7
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-06-02 00:37:10 -0300

    Gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,32 @@
+import { signIn, useSession } from 'next-auth/react';
+import { stripe } from '../../services/stripe';
 import styles from './styles.module.scss'
 
 
-interface SusbcribeButtonProps{
+interface SusbcribeButtonProps {
     priceId: string;
 
 }
-export function SubscribeButton({priceId}:SusbcribeButtonProps){
-    return(
+export function SubscribeButton({ priceId }: SusbcribeButtonProps) {
+    const  {data: session}  = useSession();
+
+
+    function handleSubscrible() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+        //criação da checkout session
+        //stripe.checkout.sessions.create
+
+    }
+
+    return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscrible}
         >
             Subscribe now
 
